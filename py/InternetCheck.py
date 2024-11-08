5a7b7ce59bad788c4648f9e72ddd0feefeadd186
TLM.dev <tlm.dev.BR@gmail.com>
2024-11-07 20:18:01 -0600

    Update InternetCheck.py

--- py/InternetCheck.py
+++ py/InternetCheck.py
@@ -1,29 +1,37 @@
+# --- Standard Library Imports --- #
 import subprocess
-import logging
-from colorama import Fore, Style
 
-ONLINE_STATUS = None
-
+# --- Local Imports --- #
 from .Global import *
 
-class iNetCheck():
+# --- Internet Connection Checker Class --- #
+class INetCheck:
+    """
+    Checks and stores the internet connection status.
+    """
+
     def __init__(self):
+        self.pingHost = None
+        self.ONLINE_STATUS = None
+
         self.launch()
 
     def launch(self):
         self.ONLINE_STATUS = self.pingCheck()
+
+        logging.info("Checking internet connection...")
+
         if self.ONLINE_STATUS:
             logging.info(f"{Fore.GREEN}Internet connection detected!{Style.RESET_ALL}")
-            return self.ONLINE_STATUS
         else:
             logging.error(f"{Fore.RED}No internet connection!{Style.RESET_ALL}")
-            return not self.ONLINE_STATUS
+
 
     def pingCheck(self):
-        pingHost = "8.8.8.8"
+        self.pingHost = "8.8.8.8"
 
         result = subprocess.run(
-            ['ping', '-c', '1', pingHost],
+            ['ping', '-c', '1', self.pingHost],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True
@@ -32,7 +40,11 @@ class iNetCheck():
         if result.returncode == 0:
             return True
         else:
-            logging.error(f"{Fore.RED}'ping' command not found.")
+            return False
 
     def isOnline(self):
         return self.ONLINE_STATUS
+
+# --- Main Execution --- #
+if __name__ == "__main__":
+    INetCheck()
